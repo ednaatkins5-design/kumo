34780910af97f472dba3770ec0c590f0ef02ff55
KUMO Deployment <deployment@kumo.local>
2026-02-24 16:24:21 +0000

    Fix logger to not use pino-pretty in production

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,18 @@
 import pino from 'pino';
 import { ENV } from '../config/env';
 
-export const logger = pino({
+const loggerConfig: any = {
     level: ENV.LOG_LEVEL,
-    transport: {
+};
+
+if (process.env.NODE_ENV !== 'production') {
+    loggerConfig.transport = {
         target: 'pino-pretty',
         options: {
             colorize: true,
             translateTime: 'SYS:standard',
         },
-    },
-});
+    };
+}
+
+export const logger = pino(loggerConfig);
